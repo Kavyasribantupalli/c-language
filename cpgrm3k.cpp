fb409b1d7f5f6e96f470a8be15f168a4e29e4cf3
Kavyasribantupalli <124046972+Kavyasribantupalli@users.noreply.github.com>
2023-06-02 10:02:57 +0530

    Add files via upload

---
+++ cpgrm3k.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>>
+int main()
+{
+
+int i,j,k,n;
+scanf("%d",&n);
+k=1;
+for(i=1;i<=n;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+		printf(" %d",k);
+		k++;
+	}
+	printf("\n");
+}
+return 0;
+}
