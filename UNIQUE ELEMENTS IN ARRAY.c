eb6d7985bf1b4012360275c10984ae0d8d1574ca
Kavyasribantupalli <124046972+Kavyasribantupalli@users.noreply.github.com>
2023-06-03 15:48:18 +0530

    Add files via upload

---
+++ UNIQUE ELEMENTS IN ARRAY.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],i,n,j,count;
+	printf("enter the array size");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]!=-1)
+		{
+		count=1;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				a[j]=-1;
+				count++;
+			}
+	    }
+	    if(count==1)
+		printf(" %d",a[i]);
+	}
+	}
+}
